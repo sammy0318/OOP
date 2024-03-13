c45bf5d5d0b153b1ff3098cf9d7c5ec6f054dd41
Venkatesh <120811365+mherenow@users.noreply.github.com>
2024-03-13 13:52:56 +0530

    Add files via upload

---
+++ Lab/Equal.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Equal {
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        System.out.print("Enter first number: ");
+        a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        b = sc.nextInt();
+        if (a == b) {
+            System.out.println("Equal");
+        } else {
+            
+            System.out.println("Not Equal");
+        }
+    }
+}
