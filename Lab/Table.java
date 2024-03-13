9fd9dbcc17c454f2417b6e8fa29532dd7dded6e4
Venkatesh <120811365+mherenow@users.noreply.github.com>
2024-03-13 13:52:17 +0530

    Create Table.java

---
+++ Lab/Table.java
@@ -0,0 +1,8 @@
+class Table {
+    public static void main(String[] args) {
+        int num = 37;
+        for (int x = 1; x <= 10; x++) {
+            System.out.println(num + " x " + x + " = " + num*x);
+        }
+    }
+}
