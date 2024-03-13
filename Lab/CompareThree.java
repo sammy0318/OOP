0570ff998b50f878460a5a95d1fa62629f3274ad
Venkatesh <120811365+mherenow@users.noreply.github.com>
2024-03-13 14:30:39 +0530

    Add files via upload

---
+++ Lab/CompareThree.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CompareThree {
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.print("Enter third number: ");
+        int c = sc.nextInt();
+
+        if (a > b && a > c) {
+            System.out.println(a + " is the greatest");
+        } else if (b > c) {
+            System.out.println(b + " is the greatest");
+        } else {
+            System.out.println(c + " is the greatest");
+        }
+
+    }
+}
