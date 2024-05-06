6b1d3ca37efe31eb417695d850343572f5a2d1b0
Venkatesh <120811365+mherenow@users.noreply.github.com>
2024-05-06 20:41:45 +0530

    Update MultipleInheritance.java
    made look good

--- Lab/MultipleInheritance.java
+++ Lab/MultipleInheritance.java
@@ -1,28 +1,25 @@
 package OOPS;
-class Mul
-{
-    int x=10;
-    
+
+class Mul {
+    int x = 10;
 }
 
-class B extends Mul
-{
-    int y=33;
+class B extends Mul {
+    int y = 33;
 }
 
-class C extends B
-{
-    int z=89;
+class C extends B {
+    int z = 89;
 }
-class MultipleInheritance
-{
+
+class MultipleInheritance {
     public static void main(String[] args) {
-        Mul a=new Mul();
-        B b=new B();
-        C c=new C();
+        Mul a = new Mul();
+        B b = new B();
+        C c = new C();
 
-        System.out.println(c.x+" "+c.y+" "+c.z);
-        System.out.println(b.x+" "+b.y);
+        System.out.println(c.x + " " + c.y + " " + c.z);
+        System.out.println(b.x + " " + b.y);
         System.out.println(a.x);
 
     }
