5189d8aaad4b222c0458cb1a079fd08d932aab9b
Somesh Pratap Singh <146454603+sammy0318@users.noreply.github.com>
2024-05-07 20:35:11 +0530

    Create UserDefinedPackage.java

---
+++ Lab-Update/UserDefinedPackage.java
@@ -0,0 +1,31 @@
+//Package 1
+package p1;
+
+public class A {
+    int x;
+    int sum(int x,int y)
+    {
+        return x+y;
+    }
+    void m()
+    {
+        System.out.println("yo yo honey singh");
+
+    }
+    
+}
+
+//Package 2 used to import package p1
+package p2;
+import p1.*;
+
+public class B {
+    public static void main(String[] args) {
+        A obj=new A();
+        obj.sum();
+        System.out.println(obj.sum(4,5));
+        obj.m();
+    }
+    
+}
+
