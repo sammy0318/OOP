3682f610e4307052fb7725b63e14f15b4f878122
Vishal Bisht <131914809+Vishal-Bisht@users.noreply.github.com>
2024-05-07 16:35:12 +0530

    Update Inheritance_16.java

--- Lab-Update/Inheritance_16.java
+++ Lab-Update/Inheritance_16.java
@@ -1,39 +1,20 @@
 class Base{
-    int X;
-    Base(){
-        System.out.println("Base class constructor");
-    }
-    Base(int a){
-        System.out.println("Overloaded Base class constructor with a = "+a);
-    }
-    public int getX(){return X;}
-    public void setX(int x){
-        this.X = x;
+    void m(){
+        System.out.println("This is parent class");
     }
 }
 
 class Derived extends Base{
-     Derived(){
-        super(5);
-        System.out.println("Derived class constructor");
-    }
-    int Y;
-
-    public int getY(){return Y;}
-    public void setY(int y){
-        this.Y = y;
+     void n(){
+        System.out.println("This is child class");
     }
 }
 
 public class Inheritance {
     public static void main(String[] args) {
         Derived d = new Derived();
-        d.setX(23);
-        d.setY(56);
-       int x = d.getX();
-       int y =  d.getY();
-        System.out.println("x = "+ x );
-        System.out.println("y = "+ y );
+        d.m();
+        d.n();
         
     }
 }
